ce0bfb220492f9980c37730272427efa79348fd3
Jhon Alexander Gomez Ruiz <jhgomez89@gmail.com>
2025-10-22 16:25:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8059115072-3707d",
+  "appId": "1:564921767769:web:64f44c3d6d5dfdf84ede48",
+  "apiKey": "AIzaSyCYSFG6qh3Ra7U-NMukLQQ1YJhyJZQC3Wc",
+  "authDomain": "studio-8059115072-3707d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "564921767769"
+};
