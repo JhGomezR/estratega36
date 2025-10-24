83173d61d4688e8d58dc9b111dfebe1951ab521c
Jhon Alexander Gomez Ruiz <jhgomez89@gmail.com>
2025-10-24 06:18:35 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/(app)/administration/users/page.tsx
+++ src/app/(app)/administration/users/page.tsx
@@ -37,6 +37,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useAuth, useCollection, useFirestore, useMemoFirebase } from "@/firebase"
